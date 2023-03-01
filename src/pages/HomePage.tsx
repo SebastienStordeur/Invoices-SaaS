242b884ca3413d414abe39791aeca26d4c473cc5
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-03-01 11:22:06 +0100

    Home conditionnal rendering

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/Forms/Login/LoginForm";
 import SignupForm from "../components/Forms/Signup/SignupForm";
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
+  const [signup, setSignup] = useState<boolean>(true);
   return (
-    <div className="text-3xl">
-      <LoginForm />
-      <SignupForm />
+    <div className="">
+      {!signup && <LoginForm />}
+      {signup && <SignupForm />}
+      <p className="text-md">
+        Already have an account?{" "}
+        <span className="text-red font-semibold cursor-pointer" onClick={() => setSignup((prev) => !prev)}>
+          {signup ? "Log In" : "Sign Up"}
+        </span>
+      </p>
     </div>
   );
 };
