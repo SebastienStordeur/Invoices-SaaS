c481f5535ad55d2bbd61375d7a0f771cd4473375
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-03-01 11:43:53 +0100

    fix rendering

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import LoginForm from "../components/Forms/Login/LoginForm";
 import SignupForm from "../components/Forms/Signup/SignupForm";
+import FormContainer from "../components/UI/FormContainer/FormContainer";
 
 const HomePage: React.FC = () => {
   const [signup, setSignup] = useState<boolean>(true);
   return (
     <div className="">
-      {!signup && <LoginForm />}
-      {signup && <SignupForm />}
-      <p className="text-md">
-        Already have an account?{" "}
-        <span className="text-red font-semibold cursor-pointer" onClick={() => setSignup((prev) => !prev)}>
-          {signup ? "Log In" : "Sign Up"}
-        </span>
-      </p>
+      <FormContainer>
+        {!signup && <LoginForm />}
+        {signup && <SignupForm />}
+        <p className="text-md text-center -mt-4">
+          {signup ? "Already have an account?" : "Need an account?"}
+          <span className="text-red font-semibold cursor-pointer" onClick={() => setSignup((prev) => !prev)}>
+            {signup ? "Log In" : "Sign Up"}
+          </span>
+        </p>
+      </FormContainer>
     </div>
   );
 };
